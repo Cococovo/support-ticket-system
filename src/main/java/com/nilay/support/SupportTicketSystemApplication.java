a6745fbe1d7c768232a588865346ce9160816a25
Cococovo <nilaymagre@gmail.com>
2026-09-05 02:30:57 +0530

    initial project setup - spring boot + postgresql connected

---
+++ src/main/java/com/nilay/support/SupportTicketSystemApplication.java
@@ -0,0 +1,14 @@
+package com.nilay.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTicketSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SupportTicketSystemApplication.class, args);
+	}
+
+}
